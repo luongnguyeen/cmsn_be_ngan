ff941603189edd50a73166ba5d421dec23a1cb6b
luongnguyeen <147536458+luongnguyeen@users.noreply.github.com>
2023-11-24 22:08:26 +0900

    Update effect.js

--- effect.js
+++ effect.js
@@ -198,13 +198,13 @@ $('document').ready(function(){
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		$('#b11').animate({top:260, left: vw-350},500);
+		$('#b22').animate({top:260, left: vw-250},500);
+		$('#b33').animate({top:260, left: vw-150},500);
+		$('#b44').animate({top:260, left: vw-50},500);
+		$('#b55').animate({top:260, left: vw+50},500);
+		$('#b66').animate({top:260, left: vw+150},500);
+		$('#b77').animate({top:260, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
