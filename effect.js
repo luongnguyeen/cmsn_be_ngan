169d2dba16d76add2bf46b0af5229f09b8a6cda6
luongnguyeen <147536458+luongnguyeen@users.noreply.github.com>
2023-11-26 15:18:20 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -24,7 +24,7 @@ $('document').ready(function(){
 		$('#bulb_pink').addClass('bulb-glow-pink');
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(4000).promise().done(function(){
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#play').fadeIn('slow');
 		});
 	});
