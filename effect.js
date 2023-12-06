f56c3d028a9f78ff7a35acb60f2000721f23ffe4
luongnguyeen <147536458+luongnguyeen@users.noreply.github.com>
2023-12-06 23:12:44 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -135,7 +135,7 @@ $('document').ready(function(){
 		function msgLoop (i) {
 			$("p:nth-child("+i+")").fadeOut('slow').delay(1000).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(5000);
+			$("p:nth-child("+i+")").fadeIn('slow').delay(4000);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').delay(2000).promise().done(function () {
 				$('#cake_fadein').fadeIn('slow');
